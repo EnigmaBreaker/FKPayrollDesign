af3952458c737ee50515203dbcfed79432b70bc3
EnigmaBreaker <cs5160398@cse.iitd.ac.in>
2020-05-10 12:34:51 +0530

    Feature01-AddEmployee 	Modified: 		 Employee.java 			- Changed data types of Employee variables 			- Defined interface functions 			- Created Employee Constructor 	TODO: 		- Implement Interface functions 		- Create methods to create new Employees

--- SourceCode/Employee.java
+++ SourceCode/Employee.java
@@ -1,14 +1,38 @@
 package projectPayroll
 
 interface EmployeeInterface {
-
+	public int getID();
+	public String getName();
+	public double getSalary();
+	public void setSalary(double salary);
+	public double getHourlyRate();
+	public void setHourlyRate(double rate);
+	public double getCommissionRate(double rate);
+	public double getDueAmount();
+	public void changeDueAmount(double amount);
 }
 
-class hourlyEmployee implements EmployeeInterface {
+class Employee implements EmployeeInterface {
 	private String name;
-	private String identity;
-	private int salary;
-	private int dueAmount;
-	private int hourlyRate;
-	private int commissionRate;
+	private final String identity;
+	private double salary;
+	private double dueAmount;
+	private double hourlyRate;
+	private double commissionRate;
+	private static int lastId = 0;
+
+	private Employee(){
+		lastId++;
+		this.identity = lastId;
+		this.dueAmount = 0;
+	}
+
+	public Employee(String name, double salary, double hourlyRate, double commissionRate){
+		this();
+		this.name = name;
+		this.salary = salary;
+		this.hourlyRate = hourlyRate;
+		this.commissionRate = commissionRate;
+	}
+
 }
